25aa7572737670f22bd68d8930950a51ad227be2
vik37 <59177404+vik37@users.noreply.github.com>
2021-03-15 00:46:13 +0100

    fifo and lifo

---
+++ Exercise_01/Exercise_17.2_QueueStack/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_17._2_QueueStack
+{
+    class Program
+    {
+         Queue<char> queueWord { get; set; } = new Queue<char>();
+         Stack<char> stackWord { get; set; } = new Stack<char>();
+         void PushCharacter(char ch)
+        {
+            stackWord.Push(ch);
+        }
+         void EnqueueCharacter(char ch)
+        {
+            queueWord.Enqueue(ch);
+        }
+         char PopCharacter()
+        {
+            char ch = stackWord.Pop();
+            return ch;
+        }
+         char DequeueCharacter()
+        {
+            char ch = queueWord.Dequeue();
+            return ch;
+        }
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            Program program = new Program();
+
+            foreach (char c in s)
+            {
+                program.PushCharacter(c);
+                program.EnqueueCharacter(c);
+            }
+
+            bool isPalindrom = true;
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                if(program.PopCharacter() != program.DequeueCharacter())
+                {
+                    isPalindrom = false;
+                    break;
+                }
+            }
+            if (isPalindrom)
+            {
+                Console.WriteLine($"The word, {s}, is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"The word, {s}, is not a palindrome.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercise_01/Exercise_17_stackQueue/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise_17_stackQueue
+{
+    class Program
+    {
+        static Queue<string> QueueNames(List<string> listNames)
+        {
+            Queue<string> names = new Queue<string>();
+            foreach (var item in listNames)
+            {
+                if(item != null)
+                {
+                    names.Enqueue(item);
+                }
+                else
+                {
+                    Console.WriteLine("List of names are empty!");
+                }
+            }
+            return names;
+        }
+        static Queue<string> DequeueNames(Queue<string> names)
+        {
+            //if(names.Contains("Cecko"))
+                names.Dequeue();
+            return names;
+        }
+        static void ShowNames(Queue<string> names)
+        {
+            int count = 0;
+            foreach (var item in names)
+            {
+                count++;
+                if(item != null)
+                {
+                    Console.WriteLine($"NAME {count}: {item}");
+                }
+            }
+        }
+        static Stack<string> AddTransferedNames(Queue<string> transNames)
+        {
+            Stack<string> transfer = new Stack<string>();
+            foreach (var item in transNames)
+            {
+                transfer.Push(item);
+            }
+            return transfer;
+        }
+        static void ShowTransferNames(Stack<string> transfers)
+        {
+            int count = 0;
+            Console.WriteLine("TRANSFER IN NEW CLAB THIS YEARS ARE:");
+            foreach (var item in transfers)
+            {
+                if(item != null)
+                {
+                    Console.WriteLine($"No. {count} - {item}");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>()
+            {
+                "Sabri",
+                "Cecko",
+                "Cedka",
+                "Ico"
+            };
+            //names.Enqueue("Sabri");
+            //names.Enqueue("Cecko");
+            //names.Enqueue("Cedka");
+            //names.Enqueue("Ico");
+
+            // Console.WriteLine($"Names: {names.Peek(1)}");
+            //Stack<string> newNames = new Stack<string>();
+            //newNames.Push("Dejan");
+            //newNames.Push("Slaganja");
+            //newNames.Push("Sasho");
+            //foreach (var item in names)
+            //{
+            //    Console.WriteLine($"Names: {item}");
+            //}
+            //foreach (var item in newNames)
+            //{
+            //    Console.WriteLine($"New Names: {item}");
+            //}
+            Queue<string> transferredNames = QueueNames(names);
+            Stack<string> finishTransfer = AddTransferedNames(transferredNames);
+            
+            DequeueNames(transferredNames);
+            DequeueNames(transferredNames);
+            DequeueNames(transferredNames);
+            DequeueNames(transferredNames);
+            ShowNames(transferredNames);
+            ShowTransferNames(finishTransfer);
+            Console.ReadLine();
+        }
+    }
+}
